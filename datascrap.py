5a67403f377154ff11ea0e407da536215eeac9c9
wisiah <49152652+wisiah@users.noreply.github.com>
2020-10-12 02:46:46 -0700

    Add files via upload

---
+++ datascrap.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests 
+
+
+url = "https://www.yellowpages.ca/search/si/1/health+wellness/Toronto+ON"
+
+
+response = requests.get(url)
+
+print(response)
+
+data = response.text
+soup = BeautifulSoup(data,'html.parser')
+items = soup.find_all('div',{'class':"listing__content__wrapper"})
+for item in items:
+    contact = item.find('h4').text[2:]
+    cname =item.find('a',{'class':"listing__name--link listing__link jsListingName"}).text[2:]
+    print('Company Name:',cname)
+    print('Çontact:',contact)
