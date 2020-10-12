a5d7ede07e53d77823e149df65e9e5c5bf0f4a4c
wisiah <49152652+wisiah@users.noreply.github.com>
2020-10-12 12:48:02 +0300

    Update datascrap.py

--- datascrap.py
+++ datascrap.py
@@ -13,7 +13,7 @@ data = response.text
 soup = BeautifulSoup(data,'html.parser')
 items = soup.find_all('div',{'class':"listing__content__wrapper"})
 for item in items:
-    contact = item.find('h4').text[2:]
+    contact = item.find('h4').text
     cname =item.find('a',{'class':"listing__name--link listing__link jsListingName"}).text[2:]
     print('Company Name:',cname)
     print('Çontact:',contact)
